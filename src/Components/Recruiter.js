663e140c131cf680989e09a8b6718320a5dd5366
Jeffrey Kingsbury <109102474+Jeffrey-Kingsbury@users.noreply.github.com>
2024-05-24 11:13:14 -0400

    buy x button on recruiter

--- src/Components/Recruiter.js
+++ src/Components/Recruiter.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Button from './Button';
 import Container from './Container';
 import { playerContext } from '../PlayerContext';
@@ -7,38 +7,78 @@ import styled from 'styled-components';
 
 const Recruiter = () => {
 	const { playerData, setPlayerData, notify } = useContext(playerContext);
+
+	const [qty, setQty] = useState(1);
 	const minerPrice = playerData.miners.miner > 0 ? (5 * Math.pow(1.07, playerData.miners.miner)).toFixed(0) : 5;
 	const excavatorPrice = playerData.miners.excavator > 0 ? (100 * Math.pow(1.07, playerData.miners.excavator)).toFixed(0) : 100;
 	const earthmoverPrice = playerData.miners.earthmover > 0 ? (20000 * Math.pow(1.07, playerData.miners.earthmover)).toFixed(0) : 20000;
+	const calcPrice = (onePrice, ownedQty, baseMultiplier) => {
+		const owned = ownedQty || 0; // Use 0 if ownedQty is undefined
+
+		let totalCost = 0;
+		for (let i = 0; i < qty; i++) {
+			const itemCost = baseMultiplier * Math.pow(1.07, owned + i);
+			totalCost += Math.round(itemCost); // Round each item's cost individually
+		}
+
+		return totalCost;
+	};
 
 	return (
 		<Container title={'recruiter'}>
 			<Wrapper>
 				<span>
-					Total Miners: {playerData.miners.miner}
+					Hire:
 					<Button
-						text={`Hire Miner (${minerPrice > 99999999999999 ? Number(minerPrice).toExponential(2) : Number(minerPrice).toLocaleString()}$)`}
+						text={'1x'}
 						onClick={() => {
-							if (playerData.wallet >= minerPrice) {
-								HIRE_MINER(playerData, setPlayerData, minerPrice, 1, 'miner');
+							setQty(1);
+						}}
+					/>
+					<Button
+						text={'25x'}
+						onClick={() => {
+							setQty(25);
+						}}
+					/>
+					<Button
+						text={'100x'}
+						onClick={() => {
+							setQty(100);
+						}}
+					/>
+				</span>
+				<span>
+					Total Miners: {playerData.miners.miner ? playerData.miners.miner : 0}
+					<Button
+						text={`Hire ${qty} Miner${qty > 1 ? 's' : ''} (${
+							calcPrice(minerPrice, playerData.miners.miner, 5) > 99999999999999
+								? Number(calcPrice(minerPrice, playerData.miners.miner, 5)).toExponential(2)
+								: Number(calcPrice(minerPrice, playerData.miners.miner, 5)).toLocaleString()
+						}$)`}
+						onClick={() => {
+							if (playerData.wallet >= calcPrice(minerPrice, playerData.miners.miner, 5)) {
+								HIRE_MINER(playerData, setPlayerData, calcPrice(minerPrice, playerData.miners.miner, 5), qty, 'miner');
 							}
 						}}
-						disabled={playerData.wallet < minerPrice}
+						disabled={playerData.wallet < calcPrice(minerPrice, playerData.miners.miner, 5)}
 					/>
 				</span>
 				{playerData.miners.miner >= 50 && (
 					<span>
-						Total Excavators: {playerData.miners.excavator}
+						Total Excavators: {playerData.miners.excavator ? playerData.miners.excavator : 0}
 						<Button
-							text={`Hire Excavator (${
-								excavatorPrice > 99999999999999 ? Number(excavatorPrice).toExponential(2) : Number(excavatorPrice).toLocaleString()
+							text={`Hire ${qty} Excavator${qty > 1 ? 's' : ''} (${
+								calcPrice(minerPrice, playerData.miners.excavator, 100) > 99999999999999
+									? Number(calcPrice(minerPrice, playerData.miners.excavator, 100)).toExponential(2)
+									: Number(calcPrice(minerPrice, playerData.miners.excavator, 100)).toLocaleString()
 							}$)`}
 							onClick={() => {
-								if (playerData.wallet >= excavatorPrice) {
-									HIRE_MINER(playerData, setPlayerData, excavatorPrice, 1, 'excavator');
+								if (playerData.wallet >= calcPrice(minerPrice, playerData.miners.excavator, 100)) {
+									HIRE_MINER(playerData, setPlayerData, calcPrice(minerPrice, playerData.miners.excavator, 100), qty, 'excavator');
 								}
 							}}
-							disabled={playerData.wallet < excavatorPrice}
+							disabled={playerData.wallet < calcPrice(minerPrice, playerData.miners.excavator, 100)}
 						/>
 					</span>
 				)}
@@ -47,15 +87,17 @@ const Recruiter = () => {
 					<span>
 						Total Earth movers: {playerData.miners.earthmover ? playerData.miners.earthmover : 0}
 						<Button
-							text={`Hire Earth mover (${
-								earthmoverPrice > 99999999999999 ? Number(earthmoverPrice).toExponential(2) : Number(earthmoverPrice).toLocaleString()
+							text={`Hire ${qty} Earth mover${qty > 1 ? 's' : ''} (${
+								calcPrice(minerPrice, playerData.miners.earthmover, 1000) > 99999999999999
+									? Number(calcPrice(minerPrice, playerData.miners.earthmover, 1000)).toExponential(2)
+									: Number(calcPrice(minerPrice, playerData.miners.earthmover, 1000)).toLocaleString()
 							}$)`}
 							onClick={() => {
-								if (playerData.wallet >= earthmoverPrice) {
-									HIRE_MINER(playerData, setPlayerData, earthmoverPrice, 1, 'earthmover');
+								if (playerData.wallet >= calcPrice(minerPrice, playerData.miners.earthmover, 1000)) {
+									HIRE_MINER(playerData, setPlayerData, calcPrice(minerPrice, playerData.miners.earthmover, 1000), qty, 'earthmover');
 								}
 							}}
-							disabled={playerData.wallet < earthmoverPrice}
+							disabled={playerData.wallet < calcPrice(minerPrice, playerData.miners.earthmover, 1000)}
 						/>
 					</span>
 				)}
